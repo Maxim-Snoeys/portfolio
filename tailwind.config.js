8027f64349391a5cc2f55afb7d6b2bdb0c8ce135
Maxim-Snoeys <r0792870@student.thomasmore.be>
2021-07-26 19:47:51 +0200

    structuur dabij

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    // purge: {
+    //     enabled: true,
+    //     content: [
+    //         './dist/*.html',
+    //         './dist/error/*.html',
+    //     ],
+    // },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            screens: {
+                '3xl': '2000px',
+            },
+            backgroundImage: theme => ({
+                'home': "url('../assets/img/laptop-backlight-dark.jpg')",
+            }),
+            maxWidth: {
+                '8xl': '85rem'
+            },
+            minHeight: {
+                '50': '50vh'
+            }
+        },
+        fontFamily: {
+            'titel': ['"Indie Flower"', 'cursive'],
+            'titelklein': ['Tourney', 'cursive']
+        }
+    },
+    variants: {
+        extend: {
+            borderWidth: ['hover', 'focus', 'active', 'visited'],
+        },
+    },
+    plugins: [
+        // ...
+        require('tailwindcss'),
+        require('autoprefixer'),
+        require('@tailwindcss/forms'),
+        // ...
+    ]
+}
